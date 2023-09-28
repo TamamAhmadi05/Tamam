6055acca7649f06b53b174fd8484f0be59df9c33
MEM <145424199+TamamAhmadi05@users.noreply.github.com>
2023-09-29 02:07:58 +0800

    Add files via upload
    biodata saya

---
+++ BIODATA.py
@@ -0,0 +1,9 @@
+print("                BIODATA")
+print("NAME                        :TAMAM AHMADI")
+print("NIM                         :5520123024")
+print("TTL                         :KEKALIK,05-04-2005")
+print("FAKULTAS                    :SAINS&TEKNIK")
+print("PRODI                       :ILMU KOMPUTER")
+print("GRADUATE OF                 :SMKN 1 NARMADA")
+print("NO.TLP                      :083870834017")
+"end"
